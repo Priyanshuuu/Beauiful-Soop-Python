264bf3f7eb524b80237c58f7f4769cd990f5733b
Priyanshu Pandey <41360062+Priyanshuuu@users.noreply.github.com>
2019-08-28 22:23:14 +0530

    Update Fetcher.py

--- Fetcher.py
+++ Fetcher.py
@@ -3,23 +3,13 @@ from bs4 import BeautifulSoup
 import operator 
 from collections import Counter 
 
-
 def start(url): 
-
-	
 	wordlist = [] 
 	source_code = requests.get(url).text 
-
-	 
 	soup = BeautifulSoup(source_code, 'html.parser') 
-
-	
 	for each_text in soup.findAll('div', {'class':'entry-content'}): 
 		content = each_text.text 
-
-		 
 		words = content.lower().split() 
-		
 		for each_word in words: 
 			wordlist.append(each_word) 
 		clean_wordlist(wordlist) 
@@ -38,7 +28,6 @@ def clean_wordlist(wordlist):
 			clean_list.append(word) 
 	create_dictionary(clean_list) 
 
-
 def create_dictionary(clean_list): 
 	word_count = {} 
 	
@@ -47,11 +36,6 @@ def create_dictionary(clean_list):
 			word_count[word] += 1
 		else: 
 			word_count[word] = 1
-			
-	
-	
-	
-	
 	c = Counter(word_count) 
 	
 	# returns the most occuring elements 
